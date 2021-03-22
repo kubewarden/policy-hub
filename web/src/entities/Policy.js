40b24a604ce7d6c286a418e625ca25ffcbe2fe5f
dleidi <dleidi@suse.com>
2021-03-22 16:20:34 +0100

    Give light and rough style to Policy items

--- web/src/entities/Policy.js
+++ web/src/entities/Policy.js
@@ -23,20 +23,35 @@ type Props = {
 const PolicyItem = (props: Props) => {
   return (
     <div className="policy-item">
-      <h3>{props.policy.name}</h3>
-      <br/>
-      <label>abstract:</label><span>{props.policy.abstract}</span>
-      <br/>
-      <label>source:</label><span><a className="link" href={props.policy.source} target="_blank" rel="noopener noreferrer">{props.policy.source}</a></span>
-      <br/>
-      <label>registry:</label><span><a className="link" href={props.policy.download.url} target="_blank" rel="noopener noreferrer">{props.policy.download.registry}</a></span>
-      <br/>
-      <label>tags:</label>{props.policy.tags.reduce((t1, t2) => <span>{t1}, {t2}</span>)}
-      <br/>
-      <label>resources:</label>{props.policy.resources.reduce((r1, r2) => <span>{r1}, {r2}</span>)}
-      <br/>
-      <label>mutation:</label><span>{props.policy.mutation ? "True" : "False"}</span>
-      <br/>
+      <div className="title">{props.policy.name}</div>
+      <div className="policy-item-field">
+        <label>abstract:</label>
+        <span>{props.policy.abstract}</span>
+      </div>
+      <div className="policy-item-field">
+        <label>source:</label><span>
+        <a className="link" href={props.policy.source} target="_blank" rel="noopener noreferrer">{props.policy.source}</a></span>
+      </div>
+      <div className="policy-item-field">
+        <label>registry:</label>
+        {
+          props.policy.download.url ?
+            <span><a className="link" href={props.policy.download.url} target="_blank" rel="noopener noreferrer">{props.policy.download.registry}</a></span>
+          : <span>registry not available</span>
+        }
+      </div>
+      <div className="policy-item-field">
+        <label>resources:</label>
+        {props.policy.resources.map(r => <span>{r}</span>)}
+      </div>
+      <div className="policy-item-field">
+        <label>mutation:</label>
+        <span>{props.policy.mutation ? "True" : "False"}</span>
+      </div>
+      <div className="policy-item-field">
+        <label>tags:</label>
+        {props.policy.tags.map(t => <p className="tag">{t}</p>)}
+      </div>
     </div>
   );
 };
