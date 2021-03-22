83ea1db1a4414287d492cf6e18c49af1e14af70f
dleidi <dleidi@suse.com>
2021-03-23 00:22:43 +0100

    Use dl,dt,dd instead

--- web/src/entities/Policy.js
+++ web/src/entities/Policy.js
@@ -29,50 +29,56 @@ const PolicyItem = (props: Props) => {
   return (
     <div className="policy-item">
       <div className="title">{props.policy.name}</div>
-      <div className="policy-item-field">
-        <label>abstract:</label>
-        <Highlight
-          text={props.policy.abstract}
-          highlight={props.abstractCriteria}
-        />
-      </div>
-      <div className="policy-item-field">
-        <a className="link"
-          href={props.policy.source}
-          target="_blank"
-          rel="noopener noreferrer">
-            <CodeIcon /> source
-        </a>
-      </div>
-      <div className="policy-item-field">
-        <label>registry:</label>
-        {
-          props.policy.download.registry ?
-            <code>{props.policy.download.registry}</code>
+      <dl>
+        <dt>abstract :</dt>
+        <dd>
+          <Highlight
+            text={props.policy.abstract}
+            highlight={props.abstractCriteria}
+          />
+        </dd>
+        <br/>
+        <dt>source :</dt>
+        <dd>
+          <a className="link"
+            href={props.policy.source}
+            target="_blank"
+            rel="noopener noreferrer">
+              <CodeIcon /> source
+          </a>
+        </dd>
+        <br/>
+        <dt>registry :</dt>
+        <dd>
+          {
+            props.policy.download.registry ?
+              <code>{props.policy.download.registry}</code>
+              : null
+          }
+          {
+            props.policy.download.url ?
+              <a className="link download"
+                href={props.policy.download.url}
+                target="_blank" rel="noopener noreferrer">
+                  <GetAppIcon />download
+              </a>
             : null
-        }
-        {
-          props.policy.download.url ?
-            <a className="link download"
-              href={props.policy.download.url}
-              target="_blank" rel="noopener noreferrer">
-                <GetAppIcon />download
-            </a>
-          : null
-        }
-      </div>
-      <div className="policy-item-field">
-        <label>resources:</label>
-        {props.policy.resources.map(r => <span key={props.policy.name + "-" + r}>{r}</span>)}
-      </div>
-      <div className="policy-item-field">
-        <label>mutation:</label>
-        <span>{props.policy.mutation ? "True" : "False"}</span>
-      </div>
-      <div className="policy-item-field">
-        <label>tags:</label>
-        {props.policy.tags.map(t => <p className="tag" key={props.policy.name + "-" + t}>{t}</p>)}
-      </div>
+          }
+        </dd>
+        <br/>
+        <dt>resources :</dt>
+        <dd>
+          {props.policy.resources.map(r => <span key={props.policy.name + "-" + r}>{r}</span>)}
+        </dd>
+        <br/>
+        <dt>mutation :</dt>
+        <dd>{props.policy.mutation ? "True" : "False"}</dd>
+        <br/>
+        <dt>tags :</dt>
+        <dd>
+          {props.policy.tags.map(t => <p className="tag" key={props.policy.name + "-" + t}>{t}</p>)}
+        </dd>
+      </dl>
     </div>
   );
 };
