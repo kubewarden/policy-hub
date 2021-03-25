57b28bcf7dcbdd737401b97f7e11c452df7cd467
dleidi <dleidi@suse.com>
2021-03-25 18:08:12 +0100

    Fix basic initial test

--- web/src/Hub.test.js
+++ web/src/Hub.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Hub from './Hub';
 
-test('renders learn react link', () => {
+test('renders Policy Hub link', () => {
   render(<Hub />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Policy Hub/i);
   expect(linkElement).toBeInTheDocument();
 });
