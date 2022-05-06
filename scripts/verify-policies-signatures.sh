dac208978a15adeee9f4c86e8bf17314e902c576
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-05-06 15:48:15 -0300

    Mark policies as untrusted.
    In the Github action run daily checking the policies signatures, updates
    the policies definition if the policies is or not properly signed. The
    Github workflow will open a PR with the policies changes.

--- scripts/verify-policies-signatures.sh
+++ scripts/verify-policies-signatures.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 POLICIES=`ls web/policies/`
-UNTRUSTED_POLICIES=()
 for policy in $POLICIES
 do
 	REGISTRY=$(jq -r ".download.registry" web/policies/${policy})
-	kwctl verify --verification-config-path .github/signature_verification_config.yaml ${REGISTRY} 2> /dev/null
+	kwctl verify --verification-config-path .github/signature_verification_config.yaml ${REGISTRY}
 	if [[ $? -ne 0 ]]; then
-		UNTRUSTED_POLICIES+=$REGISTRY
+		echo "$REGISTRY is untrusted"
+		jq ".signed |= false" web/policies/${policy} > /tmp/policy.json
+	else
+		echo "$REGISTRY is trusted"
+		jq ".signed |= true" web/policies/${policy}  > /tmp/policy.json
 	fi
+	mv /tmp/policy.json web/policies/${policy}
 done
-if [[ ${#UNTRUSTED_POLICIES[@]} -gt 0 ]]; then
-	echo "There are untrusted policies in the policy hub. Which are:"
-	for policy in ${UNTRUSTED_POLICIES[@]}
-	do
-		echo -e "\t$policy"
-	done
-	exit 1
-fi
-echo "All policies are signed and trusted."
-exit 0
