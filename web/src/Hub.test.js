2c45ea6f55c40283559b55662170730a7668e16d
Flavio Castelli <fcastelli@suse.com>
2023-01-20 16:49:28 +0100

    Archive the old project
    All the policies are now hosted on Artifact Hub
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- web/src/Hub.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Hub from './Hub';
-
-test('renders Policy Hub link', () => {
-  render(<Hub />);
-  const linkElement = screen.getByText(/Policy Hub/i);
-  expect(linkElement).toBeInTheDocument();
-});
